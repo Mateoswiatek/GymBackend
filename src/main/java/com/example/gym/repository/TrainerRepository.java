a7b31a599f1acdf02e06708e9cb2f807a0e9966e
Mateusz <mswiatek2002@gmail.com>
2024-03-13 21:04:32 +0100

    Pageable, 'applying in memory' problem Trainer

--- src/main/java/com/example/gym/repository/TrainerRepository.java
+++ src/main/java/com/example/gym/repository/TrainerRepository.java
@@ -1,6 +1,7 @@
 package com.example.gym.repository;
 
 import com.example.gym.repository.entity.Trainer;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,5 +12,5 @@ import java.util.List;
 public interface TrainerRepository extends JpaRepository<Trainer, Long> {
     @Query("select t from Trainer t " +
             "left join fetch t.events") // co dociagamy
-    List<Trainer> findAllTrainers();
+    List<Trainer> findAllTrainers(Pageable page);
 }
--- src/main/java/com/example/gym/service/TrainerService.java
+++ src/main/java/com/example/gym/service/TrainerService.java
@@ -3,6 +3,7 @@ package com.example.gym.service;
 import com.example.gym.repository.TrainerRepository;
 import com.example.gym.repository.entity.Trainer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public class TrainerService {
     private final TrainerRepository trainerRepository;
 
     public List<Trainer> getTrainers() {
-        return trainerRepository.findAllTrainers(); // zmiana
+        return trainerRepository.findAllTrainers(PageRequest.of(0, 3)); // zmiana
     }
 
     public Optional<Trainer> getTrainerById(Long id) {
