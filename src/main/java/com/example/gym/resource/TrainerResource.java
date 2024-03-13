fec8091a96730f55ebc48049bb753508cf2e3d4e
Mateusz <mswiatek2002@gmail.com>
2024-03-13 19:06:14 +0100

    by id with n+1 problem

--- src/main/java/com/example/gym/resource/TrainerResource.java
+++ src/main/java/com/example/gym/resource/TrainerResource.java
@@ -3,11 +3,14 @@ package com.example.gym.resource;
 import com.example.gym.repository.entity.Trainer;
 import com.example.gym.service.TrainerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/trainer")
@@ -15,8 +18,24 @@ import java.util.List;
 public class TrainerResource {
     private final TrainerService trainerService;
 
+    // TODO: 13.03.2024 tutaj bedziemy zwracac mniej szczegulowe informacje na temat trenerow, bedzie TrainerShortDto
     @GetMapping("")
     public List<Trainer> getTrainers() {
         return  trainerService.getTrainers();
+    }
+
+    // TODO: 13.03.2024 dodac opcje szukania po nazwie?
+    // TODO N+1 Hibernate
+    @GetMapping("/{id}")
+    public ResponseEntity<Trainer> getTrainer(@PathVariable Long id) {
+        Optional<Trainer> trainer = trainerService.getTrainerById(id);
+        /*
+        if(trainer.isPresent()){
+            return ResponseEntity.ok(trainer.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+         */
+        return trainer.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/example/gym/service/TrainerService.java
+++ src/main/java/com/example/gym/service/TrainerService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +15,9 @@ public class TrainerService {
 
     public List<Trainer> getTrainers() {
         return trainerRepository.findAll();
+    }
+
+    public Optional<Trainer> getTrainerById(Long id) {
+        return trainerRepository.findById(id);
     }
 }
